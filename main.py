b1bf19e267ea67ff6236e727edf4e370ff2cc180
Andrey Morozov <and.morozov.v@gmail.com>
2025-09-18 12:39:42 +0300

    ui(v1): add changes

--- main.py
+++ main.py
@@ -31,142 +31,47 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle("InRat Planner")
 
-        # create controller device
-        self.controller = Controller()
-
-        # create scheduler for qt application
-        # self.scheduler = QtScheduler()
-        # self.scheduler.start()
-        # self.scheduler.add_listener(self.checkGoodResultExecuteJob, EVENT_JOB_EXECUTED)
-        # self.scheduler.add_listener(self.updateTableHistory, EVENT_JOB_ADDED)
-        # self.generateJobsFromDB()
-
-        # create view for table Schedule
+        # create view for table Schedule and History
         self.tableModelSchedule = GenericTableWidget()
-        self.tableModelSchedule.setData(
-            data=[],
-            description=DESCRIPTION_COLUMN_SCHEDULE
-        )
-
+        self.tableModelSchedule.setData(data=[], description=DESCRIPTION_COLUMN_SCHEDULE)
         self.tableModelHistory = GenericTableWidget()
-        self.tableModelHistory.setData(
-            description=DESCRIPTION_COLUMN_HISTORY,
-            data=[]
-        )
+        self.tableModelHistory.setData(description=DESCRIPTION_COLUMN_HISTORY, data=[])
 
+        # add tables
         self.verticalLayoutHistory.addWidget(self.tableModelHistory)
         self.verticalLayoutSchedule.addWidget(self.tableModelSchedule)
-
-        # create view for table History
-        # self.tableModelHistory = DataTableModel(column_names=["№", "Начало записи", "Конец записи", "Cтатус", "Формат", "Частота"], data=[])
-        # self.setupTableView(self.tableViewHistory, self.tableModelHistory)
-        # self.updateTableSchedule()
 
         self.pushButtonAddSchedule.clicked.connect(self.createSchedule)
         # ToDo: self.pushButtonUpdateSchedule.clicked.connect(...)
         self.pushButtonDeleteSchedule.clicked.connect(self.deleteScheduleFromDB)
         # ToDo: self.pushButtonShowRecords.clicked.connect(...)
 
-    def checkGoodResultExecuteJob(self, event: JobExecutionEvent):
-        schedule_id = UUID(event.job_id)
+    def updateTableSchedule(self):
+        logger.info("The contents of the Schedule table have been updated.")
+        pass
 
-        # update time in table Schedule: last_record_time = next_record_time, next_record_time = next_record_time + sec_duration + interval
-        with database.engine.connect() as conn:
-            # get current "next record time", "duration", "sec repeat interval"
-            stmt = select(
-                Schedule.next_record_time, Schedule.sec_duration, Schedule.sec_repeat_interval
-            ).where(
-                Schedule.id == schedule_id
-            )
-            next_record_time, duration, interval = conn.execute(stmt).first()
+    def updateTableHistory(self):
+        logger.info("The contents of the History table have been updated.")
+        pass
 
-            # ToDo: change it later (process time !!!)
-            if next_record_time < datetime.datetime.now():
-                next_record_time = datetime.datetime.now()
-
-            stmt = update(Schedule).where(
-                Schedule.id == schedule_id
-            ).values(
-                last_record_time=next_record_time,
-                next_record_time=next_record_time + datetime.timedelta(seconds=duration + interval)
-            )
-            conn.execute(stmt)
-            conn.commit()
-
-            # create new job for new next_record_time
-            ...
-
-    def updateTableHistory(self, event):
-        print("Update table history")
-        schedule_id = UUID(event.job_id)
-
-        history_column = ["№", "Начало записи", "Конец записи", "Cтатус", "Формат", "Частота"]
-        data = []
-        with database.engine.connect() as conn:
-            stmt = select(Schedule.sec_duration, Schedule.sec_repeat_interval, Schedule.next_record_time, Schedule.format, Schedule.sampling_frequency).where(Schedule.id == schedule_id)
-            result = conn.execute(stmt)
-
-            for idx, row in enumerate(result):
-                duration = row[0]
-                interval = row[1]
-                start_time = row[2]
-                finish_time = start_time + datetime.timedelta(seconds=duration)
-                format = row[3]
-                sample_rate = row[4]
-                status = "Выполняется"
-
-                data.append([idx, start_time, finish_time, status, format, sample_rate])
-
-        self.tableModelSchedule = DataTableModel(column_names=history_column, data=data)
-        self.setupTableView(self.tableViewSchedule, self.tableModelSchedule)
-
-    def generateJobsFromDB(self):
-        # get schedule data from db
-        with database.engine.connect() as conn:
-            stmt = select(Schedule)
-            result = conn.execute(stmt)
-            schedules = [dict(row._mapping) for row in result]
-
-        # add schedule as job in QtScheduler
-        for sched in schedules:
-            start_time = sched["next_record_time"]
-
-            if not isinstance(start_time, datetime.datetime):
-                raise TypeError("Type is not datetime!")
-
-            # ToDo: change it later
-            if start_time < datetime.datetime.now():
-                start_time = datetime.datetime.now()
-
-            logger.debug(
-                f"Add new job from table Schedule: "
-                f"id={sched['id']}, start_time={sched['next_record_time']}, duration={sched['sec_duration']} sec"
-            )
-            # self.scheduler.add_job(
-            #     func=self.controller.start_recording, trigger="date",
-            #     args=(sched["device_sn"], sched['next_record_time'], sched["sec_duration"]),
-            #     id=str(sched["id"]), run_date=start_time,
-            # )
-
+    def init_scheduler(self):
+        # create controller device
+        # self.controller = Controller()
+        # create scheduler for qt application
+        # self.scheduler = QtScheduler()
+        # self.scheduler.start()
+        # self.scheduler.add_listener(self.checkGoodResultExecuteJob, EVENT_JOB_EXECUTED)
+        # self.scheduler.add_listener(self.updateTableHistory, EVENT_JOB_ADDED)
+        # self.generateJobsFromDB()
+        pass
 
     def createSchedule(self) -> None:
         dlg = DlgCreateSchedule()
         code = dlg.exec()
         if code == QDialog.DialogCode.Accepted:
             schedule = dlg.getSchedule()
-
             if schedule is None:
                 return
-
-            schedule_id = self._addScheduleIntoDB(schedule)
-            # for example
-            # add job in schedule
-            # self.scheduler.add_job(
-            #     func=self.controller.start_recording, args=(schedule["start_time"], schedule["duration"]),
-            #     trigger="date", id=schedule_id, run_date=schedule["start_time"],
-            #     next_run_time=schedule["start_time"] + datetime.timedelta(seconds=schedule["duration"] + schedule["interval"])
-            # )
-
 
     def _addScheduleIntoDB(self, schedule: dict) -> str:
         logger.debug(f"Add in DB new schedule: {schedule=}")
@@ -186,21 +91,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             conn.commit()
         self.updateTableSchedule()
         return str(schedule_id)
-
-    def updateTableSchedule(self):
-        data = []
-        with database.engine.connect() as conn:
-            stmt = select(
-                Schedule.id,
-                Schedule.patient, Schedule.device_sn,
-                Schedule.sampling_frequency, Schedule.format,
-                Schedule.sec_duration, Schedule.sec_repeat_interval
-            )
-            rows = conn.execute(stmt)
-            for idx, row in enumerate(rows):
-                data.append([idx + 1, *row])
-        self.tableModelSchedule = DataTableModel(column_names=["№", "id", "Имя объекта", "Серийный номер", "Частота", "Формат", "Длительность", "Интервал", ], data=data)
-        self.setupTableView(self.tableViewSchedule, self.tableModelSchedule)
 
     def deleteScheduleFromDB(self):
         index = self.tableViewSchedule.currentIndex()
--- widgets.py
+++ widgets.py
@@ -4,7 +4,7 @@ from xml.dom import NoModificationAllowedErr
 
 from PySide6.QtCore import QDateTime
 from PySide6.QtGui import QPalette, QColor
-from PySide6.QtWidgets import QDialog, QComboBox, QSpinBox, QDialogButtonBox, QPushButton
+from PySide6.QtWidgets import QDialog, QComboBox, QSpinBox, QDialogButtonBox, QPushButton, QMessageBox
 
 from ui.v1.dlg_input_schedule import Ui_DlgCreateNewSchedule
 
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 class DlgCreateSchedule(Ui_DlgCreateNewSchedule, QDialog):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, experiments: Optional[list] = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
@@ -21,32 +21,46 @@ class DlgCreateSchedule(Ui_DlgCreateNewSchedule, QDialog):
         self.dateTimeEditStartExperiment.setCalendarPopup(True)
         self.dateTimeEditFinishExperiment.setCalendarPopup(True)
 
+        if experiments is not None:
+            self.ComboBoxExperiment.addItems(experiments)
+
+        # fill combobox
+        self.ComboBoxExperiment.setCurrentText("Не выбрано")
         self.ComboBoxExperiment.setEditable(True)
-
-        # setup combobox model device
         self.ComboBoxModelDevice.addItems(["InRat", "EMGsens"])
-        # setup combobox sampling rate
         self.comboBoxFreq.addItems(["500 Гц", "1000 Гц", "2000 Гц"])
-        # setup combobox saving format
-        self.comboBoxFormat.addItems(
-            ["Comma-separated values (CSV)", "European Data Format (EDF)", "Waveform Database (WFDB)"]
-        )
+        self.comboBoxFormat.addItems(["Comma-separated values (CSV)", "European Data Format (EDF)", "Waveform Database (WFDB)"])
+        self.comboBoxDuration.addItems(["01:00", "02:00", "03:00", "04:00", "05:00", "10:00", "15:00", "20:00"])
+        self.comboBoxDuration.setPlaceholderText("[mm:ss]")
+        self.comboBoxInterval.addItems(["01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "12:00", "24:00", "48:00"])
+        self.comboBoxInterval.setPlaceholderText("[hh:mm]")
 
-        self.setDefaults()
+        # rename buttons
+        self.buttonBoxSchedule.button(QDialogButtonBox.StandardButton.Ok).setText("Ок")
+        self.buttonBoxSchedule.button(QDialogButtonBox.StandardButton.Cancel).setText("Отменить")
+        self.buttonBoxSchedule.button(QDialogButtonBox.StandardButton.RestoreDefaults).setText("По умолчанию")
+
         self.buttonBoxSchedule.button(QDialogButtonBox.StandardButton.Ok).clicked.connect(self.accept)
         self.buttonBoxSchedule.button(QDialogButtonBox.StandardButton.Cancel).clicked.connect(self.reject)
         self.buttonBoxSchedule.button(QDialogButtonBox.StandardButton.RestoreDefaults).clicked.connect(self.setDefaults)
+
+        self.setDefaults()
+
 
     def setDefaults(self):
         logger.info("Set default settings for schedule")
 
         self.comboBoxFormat.setCurrentIndex(1)
-        self.ComboBoxExperiment.setCurrentText("Не выбрано")
+        # self.ComboBoxExperiment.setCurrentText("Не выбрано")
         self.ComboBoxModelDevice.setCurrentIndex(1)
         self.comboBoxFreq.setCurrentIndex(1)
 
+        self.ComboBoxExperiment.setCurrentIndex(-1)
+        self.comboBoxDuration.setCurrentIndex(-1)
+        self.comboBoxInterval.setCurrentIndex(-1)
+
         self.dateTimeEditStartExperiment.setDateTime(QDateTime.currentDateTime().addSecs(60))
-        self.dateTimeEditFinishExperiment.setDateTime(QDateTime.currentDateTime().addMonths(1))
+        self.dateTimeEditFinishExperiment.setDateTime(QDateTime.currentDateTime().addDays(1))
 
         self.dateTimeEditStartExperiment.setMinimumDateTime(QDateTime.currentDateTime().addSecs(60))
         self.dateTimeEditFinishExperiment.setMinimumDateTime(QDateTime.currentDateTime().addSecs(60))
@@ -68,12 +82,13 @@ class DlgCreateSchedule(Ui_DlgCreateNewSchedule, QDialog):
         # sec_duration = self.convertTimeIntoSeconds(combobox=self.comboBoxDurationDim, spinbox=self.spinBoxDuration)
         # sec_interval = self.convertTimeIntoSeconds(combobox=self.comboBoxIntervalDim, spinbox=self.spinBoxInterval)
 
-        return {
-            "experiment": experiment, "object": patient,
-            "device_sn": device_sn, "device_model": device_model,
-            "duration": duration, "interval_repat": interval_repeat, "sampling_rate": sampling_rate, "format": save_format,
-            "start_time": start_datetime_experiment, "finish_time": finish_datetime_experiment,
-        }
+        # return {
+        #     "experiment": experiment, "object": patient,
+        #     "device_sn": device_sn, "device_model": device_model,
+        #     "duration": duration, "interval_repat": interval_repeat, "sampling_rate": sampling_rate, "format": save_format,
+        #     "start_time": start_datetime_experiment, "finish_time": finish_datetime_experiment,
+        # }
+        return None
 
     @classmethod
     def convertTimeIntoSeconds(cls, combobox: QComboBox, spinbox: QSpinBox) -> int:
