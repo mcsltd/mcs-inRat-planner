3c19a23517a8cd82e43f8fd1d161ced2e505e15f
Andrey Morozov <and.morozov.v@gmail.com>
2025-11-05 13:59:01 +0300

    ui: recover old version ui

--- resources/v1/dlg_input_schedule.py
+++ resources/v1/dlg_input_schedule.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'dlg_input_scheduleYxzJZX.ui'
+## Form generated from reading UI file 'dlg_input_scheduleqVTZMc.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
