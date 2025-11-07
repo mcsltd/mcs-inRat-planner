319fe8c64b6933187839728acfa9d69f20da97c2
Andrey Morozov <and.morozov.v@gmail.com>
2025-11-07 13:02:17 +0300

    ui: add ui in main settings for recover and delete archive data

--- ui/settings_dialog.py
+++ ui/settings_dialog.py
@@ -1,29 +1,28 @@
 from PySide6.QtCore import QModelIndex
 from PySide6.QtGui import QFont, Qt
-from PySide6.QtWidgets import QDialog, QVBoxLayout, QWidget, QPushButton, QHBoxLayout, QGroupBox, QSpinBox, QLabel, \
-    QSpacerItem, QSizePolicy
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QWidget, QHBoxLayout, QGroupBox, QSpinBox, QLabel, \
+    QSpacerItem, QSizePolicy, QPushButton
 
-from db.database import connection
-from db.models import Device, Experiment, Schedule, Object
 from resources.v1.frm_localConfig import Ui_FrmMainConfig
-from tools.modview import GenericTableWidget
 
 
 class DlgMainConfig(QDialog, Ui_FrmMainConfig):
     """ Главное окно настроек """
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
 
         self._idx_selected_widget = 0
         self.widgets = [
-            WidgetCfgDevice(),
-            # WidgetCfgExperiment(),
-            # WidgetCfgObject()
+            WidgetCfgGeneral(),
         ]
         self.set_widgets()
 
         self.listWidget.clicked.connect(self.setup_widget)
+
+        self.pushButtonCancel.clicked.connect(self.close)
 
     def setup_widget(self, index: QModelIndex):
         model_idx = self.listWidget.selectedIndexes()[0]
@@ -62,14 +61,6 @@ class WidgetCfg(QWidget):
         self.label = QLabel(self.name, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
         self.label.setFont(self.font)
 
-        # self.table = GenericTableWidget()
-        # self.btnAdd = QPushButton("Добавить")
-        # self.horizontalLayoutControlTable.addWidget(self.btnAdd)
-        # self.btnUpdate = QPushButton("Изменить")
-        # self.horizontalLayoutControlTable.addWidget(self.btnUpdate)
-        # self.btnDelete = QPushButton("Удалить")
-        # self.horizontalLayoutControlTable.addWidget(self.btnDelete)
-
         self.verticalLayout.addWidget(self.label)
         # self.verticalLayout.addWidget(self.table)
         self.verticalLayout.addLayout(self.horizontalLayoutControlTable)
@@ -89,32 +80,30 @@ class WidgetCfgGeneral(WidgetCfg):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = "Конфигурация BLE"
+        self.name = "Общее"
         self.setup_ui()
         self.setup_ble_settings()
+        self.archive_recovery_ui()
+        self.archive_remove_ui()
+        self.verticalLayout.addStretch()  # Растягивающийся spacer
+
 
     def setup_ble_settings(self):
         """Настройка интерфейса для BLE настроек"""
-
-        # Группа настроек подключения BLE
         self.ble_connection_group = QGroupBox("Настройки подключения BLE устройств")
         ble_connection_layout = QVBoxLayout(self.ble_connection_group)
-
-        # Настройка максимального количества подключений
         connection_limit_layout = QHBoxLayout()
         self.label_cnt_device = QLabel("Максимальное количество подключаемых устройств:")
         connection_limit_layout.addWidget(self.label_cnt_device)
-
         self.connection_spinbox = QSpinBox()
         self.connection_spinbox.setRange(1, 4)
         self.connection_spinbox.setValue(2)
         self.connection_spinbox.setSuffix(" устройств")
         self.connection_spinbox.setFixedWidth(120)
         connection_limit_layout.addWidget(self.connection_spinbox)
-
         connection_limit_layout.addStretch()
         ble_connection_layout.addLayout(connection_limit_layout)
-
+        self.v_spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
         # Поясняющая надпись
         info_label = QLabel("• Увеличение количества может повлиять на стабильность работы")
         info_label.setStyleSheet("color: #666666; font-size: 10px; font-style: italic;")
@@ -122,12 +111,44 @@ class WidgetCfgGeneral(WidgetCfg):
 
         self.verticalLayout.addWidget(self.ble_connection_group)
 
-        # Добавляем растягивающий элемент в конец
-        # self.verticalLayout.addStretch()
+    def archive_recovery_ui(self):
+        """ Интерфейс восстановления архивированных данных """
+        self.restore_group = QGroupBox("Восстановление архивных расписаний и записей")
+        restore_layout = QVBoxLayout(self.restore_group)
+        self.label_archive_info = QLabel("Всего архивированных расписаний: 0")
 
+        info_layout = QHBoxLayout()
+        info_layout.addWidget(self.label_archive_info)
+        info_layout.addStretch()
+        restore_layout.addLayout(info_layout)
 
-class WidgetCfgDevice(WidgetCfg):
-    """ Класс для настройки устройств """
+        button_layout = QHBoxLayout()
+        self.btn_restore_all = QPushButton("Восстановить всё")
+
+        button_layout.addWidget(self.btn_restore_all)
+        button_layout.addStretch()
+        restore_layout.addLayout(button_layout)
+
+        # Добавляем группы в основной layout
+        self.verticalLayout.addWidget(self.restore_group)
+
+    def archive_remove_ui(self):
+        """ Интерфейс удаления архивированных данных """
+        # Группа удаления данных
+        self.delete_group = QGroupBox("Удаление архивных расписаний и записей")
+        delete_layout = QVBoxLayout(self.delete_group)
+
+        button_layout = QHBoxLayout()
+        self.btn_delete = QPushButton("Удалить всё")
+
+        button_layout.addWidget(self.btn_delete)
+        button_layout.addStretch()
+        delete_layout.addLayout(button_layout)
+
+        # Добавляем группы в основной layout
+        self.verticalLayout.addWidget(self.delete_group)
+
+"""class WidgetCfgDevice(WidgetCfg):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = "Устройства"
@@ -144,10 +165,8 @@ class WidgetCfgDevice(WidgetCfg):
             d = d.to_dataclass()
             data.append([idx + 1, d.ble_name])
 
-        # self.table.setData(data=data, description=columns)
 
     def setup_ble_settings(self):
-        """Настройка интерфейса для BLE настроек"""
         # Группа настроек подключения BLE
         self.ble_connection_group = QGroupBox("Настройки подключения BLE устройств")
         ble_connection_layout = QVBoxLayout(self.ble_connection_group)
@@ -175,28 +194,8 @@ class WidgetCfgDevice(WidgetCfg):
         ble_connection_layout.addWidget(info_label)
 
         self.verticalLayout.insertWidget(1, self.ble_connection_group)
-        self.verticalLayout.insertItem(2, self.v_spacer)
-
-class WidgetCfgExperiment(WidgetCfg):
-    """ Класс для настройки экспериментов """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = "Эксперименты"
-        self.setup_ui()
-        self.set_data_table()
-
-    @connection
-    def set_data_table(self, session):
-        data = []
-        columns = ["№", "Название"]
-        experiments = Experiment.fetch_all(session)
-        for idx, exp in enumerate(experiments):
-            exp = exp.to_dataclass()
-            data.append([idx + 1, exp.name])
-        # self.table.setData(data=data, description=columns)
-
-class WidgetCfgSchedule(WidgetCfg):
-    """ Класс для настройки расписаний """
+        self.verticalLayout.insertItem(2, self.v_spacer)"""
+"""class WidgetCfgSchedule(WidgetCfg):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = "Расписания"
@@ -217,27 +216,43 @@ class WidgetCfgSchedule(WidgetCfg):
                          self.convert_seconds_to_str(sch.sec_interval),
                          self.convert_seconds_to_str(sch.sec_duration)]
             )
+        # self.table.setData(data=data, description=columns)"""
+"""
+class WidgetCfgObject(WidgetCfg):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = "Объекты"
+        self.setup_ui()
+        self.set_data_table()
+
+    @connection
+    def set_data_table(self, session):
+        data = []
+        columns = ["№", "Название"]
+        objs = Object.fetch_all(session)
+        for idx, obj in enumerate(objs):
+            obj = obj.to_dataclass()
+            data.append([idx + 1, obj.name])
         # self.table.setData(data=data, description=columns)
+"""
+"""
+class WidgetCfgExperiment(WidgetCfg):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = "Эксперименты"
+        self.setup_ui()
+        self.set_data_table()
 
-# class WidgetCfgObject(WidgetCfg):
-#     """ Класс для настройки объектов исследования """
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.name = "Объекты"
-#         self.setup_ui()
-#         self.set_data_table()
-#
-#     @connection
-#     def set_data_table(self, session):
-#         data = []
-#         columns = ["№", "Название"]
-#         objs = Object.fetch_all(session)
-#         for idx, obj in enumerate(objs):
-#             obj = obj.to_dataclass()
-#             data.append([idx + 1, obj.name])
-#         # self.table.setData(data=data, description=columns)
-
-
+    @connection
+    def set_data_table(self, session):
+        data = []
+        columns = ["№", "Название"]
+        experiments = Experiment.fetch_all(session)
+        for idx, exp in enumerate(experiments):
+            exp = exp.to_dataclass()
+            data.append([idx + 1, exp.name])
+        # self.table.setData(data=data, description=columns)
+"""
 
 if __name__ == "__main__":
     from PySide6.QtWidgets import QApplication, QLabel, QWidget, QDialog
