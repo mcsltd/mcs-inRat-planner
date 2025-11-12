46da9664617059110ad221928eed53db4832a28a
Andrey Morozov <and.morozov.v@gmail.com>
2025-11-12 14:20:27 +0300

    ui: update dialog window for experiment

--- resources/v1/dlg_input_experiment.py
+++ resources/v1/dlg_input_experiment.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'designerYRpPSi.ui'
+## Form generated from reading UI file 'dlg_input_experimentOCjFff.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
@@ -16,43 +16,51 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
-    QFormLayout, QLabel, QLineEdit, QSizePolicy,
-    QWidget)
+    QFormLayout, QGridLayout, QLabel, QLineEdit,
+    QSizePolicy, QWidget)
 
 class Ui_DlgInputExperiment(object):
     def setupUi(self, DlgInputExperiment):
         if not DlgInputExperiment.objectName():
             DlgInputExperiment.setObjectName(u"DlgInputExperiment")
         DlgInputExperiment.setWindowModality(Qt.WindowModality.ApplicationModal)
-        DlgInputExperiment.resize(400, 153)
+        DlgInputExperiment.resize(400, 154)
         font = QFont()
         font.setFamilies([u"Segoe UI Emoji"])
         font.setPointSize(11)
         DlgInputExperiment.setFont(font)
-        self.buttonBox = QDialogButtonBox(DlgInputExperiment)
-        self.buttonBox.setObjectName(u"buttonBox")
-        self.buttonBox.setGeometry(QRect(20, 90, 341, 32))
-        self.buttonBox.setFont(font)
-        self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
-        self.formLayoutWidget = QWidget(DlgInputExperiment)
-        self.formLayoutWidget.setObjectName(u"formLayoutWidget")
-        self.formLayoutWidget.setGeometry(QRect(30, 50, 341, 31))
-        self.formLayoutWidget.setFont(font)
-        self.formLayoutExperiment = QFormLayout(self.formLayoutWidget)
-        self.formLayoutExperiment.setObjectName(u"formLayoutExperiment")
-        self.formLayoutExperiment.setContentsMargins(0, 0, 0, 0)
-        self.labelExperiment = QLabel(self.formLayoutWidget)
+        self.gridLayout = QGridLayout(DlgInputExperiment)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.labelExperiment = QLabel(DlgInputExperiment)
         self.labelExperiment.setObjectName(u"labelExperiment")
-        self.labelExperiment.setFont(font)
+        font1 = QFont()
+        font1.setFamilies([u"Segoe UI"])
+        font1.setPointSize(11)
+        self.labelExperiment.setFont(font1)
 
-        self.formLayoutExperiment.setWidget(0, QFormLayout.ItemRole.LabelRole, self.labelExperiment)
+        self.gridLayout.addWidget(self.labelExperiment, 0, 0, 1, 1)
 
-        self.lineEditExperiment = QLineEdit(self.formLayoutWidget)
+        self.formLayoutExperiment = QFormLayout()
+        self.formLayoutExperiment.setObjectName(u"formLayoutExperiment")
+        self.lineEditExperiment = QLineEdit(DlgInputExperiment)
         self.lineEditExperiment.setObjectName(u"lineEditExperiment")
         self.lineEditExperiment.setFont(font)
 
         self.formLayoutExperiment.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lineEditExperiment)
+
+
+        self.gridLayout.addLayout(self.formLayoutExperiment, 1, 0, 1, 1)
+
+        self.buttonBox = QDialogButtonBox(DlgInputExperiment)
+        self.buttonBox.setObjectName(u"buttonBox")
+        font2 = QFont()
+        font2.setFamilies([u"Segoe UI Emoji"])
+        font2.setPointSize(9)
+        self.buttonBox.setFont(font2)
+        self.buttonBox.setOrientation(Qt.Orientation.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
+
+        self.gridLayout.addWidget(self.buttonBox, 2, 0, 1, 1)
 
 
         self.retranslateUi(DlgInputExperiment)
@@ -63,7 +71,7 @@ class Ui_DlgInputExperiment(object):
     # setupUi
 
     def retranslateUi(self, DlgInputExperiment):
-        DlgInputExperiment.setWindowTitle(QCoreApplication.translate("DlgInputExperiment", u"\u0414\u043e\u0431\u0430\u0432\u043b\u0435\u043d\u0438\u0435 \u044d\u043a\u0441\u043f\u0435\u0440\u0438\u043c\u0435\u043d\u0442\u0430", None))
-        self.labelExperiment.setText(QCoreApplication.translate("DlgInputExperiment", u"\u042d\u043a\u0441\u043f\u0435\u0440\u0438\u043c\u0435\u043d\u0442", None))
+        DlgInputExperiment.setWindowTitle(QCoreApplication.translate("DlgInputExperiment", u"\u0421\u043e\u0437\u0434\u0430\u043d\u0438\u0435 \u044d\u043a\u0441\u043f\u0435\u0440\u0438\u043c\u0435\u043d\u0442\u0430", None))
+        self.labelExperiment.setText(QCoreApplication.translate("DlgInputExperiment", u"\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u043d\u0430\u0437\u0432\u0430\u043d\u0438\u0435 \u044d\u043a\u0441\u043f\u0435\u0440\u0438\u043c\u0435\u043d\u0442\u0430:", None))
     # retranslateUi
 
